abfb7e7a43186ad45e99fc55753aa318473261a2
Faraax <suny.faraz@gmail.com>
2023-05-13 01:22:43 +0500

    Commit

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,21 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json({ msg: "working" })
+    // res.json({ msg: "working" })
+    const locals = {
+        title: "Nodejs Blog || Home",
+        description: "Simple Blog created with Nodejs, Express & MongoDB."
+    }
+    res.render('index', { locals })
+})
+
+router.get('/about', (req, res) => {
+    const locals = {
+        title: "Nodejs Blog || About",
+        description: "Simple Blog created with Nodejs, Express & MongoDB."
+    }
+    // res.json({ msg: "working" })
+    res.render('about', { locals })
 })
 
 module.exports = router
